26b39c8e9c8a48cdf39d1aa76d00e04eee9d0d0d
MayankUjawane <mayank302000@gmail.com>
2022-09-28 11:25:20 +0000

    add entities

---
+++ springapp/src/main/java/ai/iamneo/springapp/entity/Product.java
@@ -0,0 +1,70 @@
+package ai.iamneo.springapp.entity;
+
+public class Product {
+    private String productId;
+    private String imageUrl;
+    private String productName;
+    private String price;
+    private String description;
+    private String quantity;
+    
+    public Product() {}
+
+    public Product(String productId, String imageUrl, String productName, String price, String description,
+            String quantity) {
+        this.productId = productId;
+        this.imageUrl = imageUrl;
+        this.productName = productName;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+}
